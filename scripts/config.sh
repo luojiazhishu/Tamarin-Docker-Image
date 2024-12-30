2076d65065f54b9d8d8055d332c6d9aafe8f6cb5
OccDeser <2287109950@qq.com>
2024-12-30 14:15:29 +0800

    Add version $1.2.0$

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,2 +1,3 @@
-TAMARIN_VERSION="1.10.0"
-TAMARIN_RELEASE_URL="https://github.com/tamarin-prover/tamarin-prover/releases/download/1.10.0/tamarin-prover-1.10.0-linux64-ubuntu.tar.gz"
+TAMARIN_VERSION="1.2.0"
+TAMARIN_RELEASE_URL="https://github.com/tamarin-prover/tamarin-prover/releases/download/1.2.0/tamarin-prover-1.2-linux64.tar.gz"
+TAMARIN_RELEASE_BOTTLE="false"
--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,9 @@
-# Download tamarin-prover
+# Download tamarin-prover and download
 source ./scripts/config.sh
+
 wget $TAMARIN_RELEASE_URL -O tamarin-prover.tar.gz
 tar -xzf tamarin-prover.tar.gz 
-rm tamarin-prover.tar.gz
+
+if [ -f tamarin-prover-linux ]; then
+    mv tamarin-prover-linux tamarin-prover
+fi
