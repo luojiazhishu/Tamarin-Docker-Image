455c83cf4e0a371dc5e5c4236ec7f143397a3b37
OccDeser <2287109950@qq.com>
2024-12-30 14:15:37 +0800

    Add version $1.4.0$

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,3 @@
-TAMARIN_VERSION="1.2.3"
-TAMARIN_RELEASE_URL="https://github.com/tamarin-prover/tamarin-prover/releases/download/1.2.3/tamarin-prover-1.2.3-linux64-ubuntu.tar.gz"
+TAMARIN_VERSION="1.4.0"
+TAMARIN_RELEASE_URL="https://github.com/tamarin-prover/tamarin-prover/releases/download/1.4.0/tamarin-prover-1.4.0-linux64-ubuntu.tar.gz"
 TAMARIN_RELEASE_BOTTLE="false"
